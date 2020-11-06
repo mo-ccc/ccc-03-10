bb8ad84fe79e94dad707f9183b748db4ef54bc7b
EC2 Default User <ec2-user@ip-172-31-2-80.ec2.internal>
2020-11-06 05:58:07 +0000

    fully functional crud api finished

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -11,47 +11,38 @@ def book_index():
     print(books)
     return jsonify(books_schema.dump(books))
 
-# @books.route("/", methods=["POST"])
-# def book_create():
-#     #Create a new book
-#     sql = "INSERT INTO books (title) VALUES (%s);"
-#     cursor.execute(sql, (request.json["title"],))
-#     connection.commit()
-
-#     sql = "SELECT * FROM books ORDER BY ID DESC LIMIT 1"
-#     cursor.execute(sql)
-#     book = cursor.fetchone()
-#     return jsonify(book)
-
-# @books.route("/<int:id>", methods=["GET"])
-# def book_show(id):
-#     #Return a single book
-#     sql = "SELECT * FROM books WHERE id = %s;"
-#     cursor.execute(sql, (id,))
-#     book = cursor.fetchone()
-#     return jsonify(book)
-
-# @books.route("/<int:id>", methods=["PUT", "PATCH"])
-# def book_update(id):
-#     #Update a book
-#     sql = "UPDATE books SET title = %s WHERE id = %s;"
-#     cursor.execute(sql, (request.json["title"], id))
-#     connection.commit()
-
-#     sql = "SELECT * FROM books WHERE id = %s"
-#     cursor.execute(sql, (id,))
-#     book = cursor.fetchone()
-#     return jsonify(book)
-
-# @books.route("/<int:id>", methods=["DELETE"])
-# def book_delete(id):
-#     sql = "SELECT * FROM books WHERE id = %s;"
-#     cursor.execute(sql, (id,))
-#     book = cursor.fetchone()
+@books.route("/", methods=["POST"])
+def book_create():
+    #Create a new book
+    data = book_schema.load(request.json)
     
-#     if book:
-#         sql = "DELETE FROM books WHERE id = %s;"
-#         cursor.execute(sql, (id,))
-#         connection.commit()
+    new_book = Book()
+    new_book.title = data["title"]
+    
+    db.session.add(new_book)
+    db.session.commit()
+    
+    return jsonify(books_schema.dump(Book.query.all()))
 
-#     return jsonify(book)
+@books.route("/<int:id>", methods=["GET"])
+def book_show(id):
+    #Return a single book
+    book = Book.query.get(id)
+    return jsonify(book_schema.dump(book))
+
+@books.route("/<int:id>", methods=["PUT", "PATCH"])
+def book_update(id):
+    #Update a book
+    data = book_schema.load(request.json)
+    book = Book.query.get(id)
+    book.title = data["title"]
+    db.session.commit()
+    return jsonify(books_schema.dump(Book.query.all()))
+    
+
+@books.route("/<int:id>", methods=["DELETE"])
+def book_delete(id):
+    book = Book.query.get(id)
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify(books_schema.dump(Book.query.all()))
