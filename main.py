5159777ff011e4170e5ac8fbb92e4d374c4d2c2d
EC2 Default User <ec2-user@ip-172-31-2-80.ec2.internal>
2020-11-12 01:33:07 +0000

    added validation

--- main.py
+++ main.py
@@ -20,3 +20,10 @@ app.register_blueprint(db_commands)
 from controllers import registerable_controllers
 for controller in registerable_controllers:
     app.register_blueprint(controller)
+
+from flask import jsonify
+from marshmallow.exceptions import ValidationError
+
+@app.errorhandler(ValidationError)
+def handle_bad_request(error):
+    return (jsonify(error.messages), 400)
