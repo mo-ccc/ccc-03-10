933e9068156be9874a2eef97ff45191d008539f3
EC2 Default User <ec2-user@ip-172-31-2-80.ec2.internal>
2020-11-14 05:37:06 +0000

    s3 bucket static files implemented

--- controllers/book_images_controller.py
+++ controllers/book_images_controller.py
@@ -1,9 +1,13 @@
 from models.BookImage import BookImage
 from models.Book import Book
+from main import db
+from pathlib import Path
+import flask
 from schemas.BookImageSchema import book_image_schema
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from flask import Blueprint, request, jsonify, abort
+from flask import Blueprint, request, jsonify, abort, send_from_directory
 from services.auth_service import verify_user
+import boto3
 
 book_images = Blueprint("book_images", __name__, url_prefix="/books/<int:book_id>/image")
 
@@ -11,15 +15,53 @@ book_images = Blueprint("book_images", __name__, url_prefix="/books/<int:book_id
 @jwt_required
 @verify_user
 def book_image_create(book_id, user=None):
-    return "1"
+    book = Book.query.filter_by(id=book_id, user_id=get_jwt_identity()).first()
+    
+    if not book:
+        return abort(401, description="unauthorized")
+    
+    if "image" not in request.files:
+        return abort(401, description="No image")
+        
+    image = request.files["image"]
+        
+    if Path(image.filename).suffix not in {".png", ".jpg", ".jpeg"}:
+        return abort(401, description="not a png")
+    
+    filename = f"{book_id}{Path(image.filename).suffix}"
+    bucket = boto3.resource("s3").Bucket(flask.current_app.config["AWS_S3_BUCKET"])
+    
+    key = f"book_image/{filename}"
+    
+    bucket.upload_fileobj(image, key)
+    
+    if not(book.book_image):
+        new_image = BookImage()
+        new_image.filename = filename 
+        book.book_image = new_image
+        db.session.commit()
+    
+    return ("", 200)
 
-@book_images.route("/<int:id>", methods=["GET"])
-@jwt_required
-@verify_user
-def book_image_show(book_id, id, user=None):
-    return "2"
+@book_images.route("/", methods=["GET"])
+def book_image_show(book_id, user=None):
+    book_image = BookImage.query.filter_by(book_id=book_id).first()
+    if not book_image:
+        return abort(404, "No image")
+    
+    bucket = boto3.resource("s3").Bucket(flask.current_app.config["AWS_S3_BUCKET"])
+    
+    filename = book_image.filename
+    
+    file_obj = bucket.Object(f"book_image/{filename}").get()
+    
+    return flask.Response(
+        file_obj["Body"].read(),
+        mimetype="image/*",
+        headers={"Content-Disposition": "attachment;filename=image"}
+    )
 
-@book_images.route("/<int:id>", methods=["DELETE"])
+@book_images.route("/", methods=["DELETE"])
 @jwt_required
-def book_image_delete(book_id, id, user=None):
+def book_image_delete(book_id, user=None):
     return "3"
--- default_settings.py
+++ default_settings.py
@@ -6,7 +6,8 @@ dotenv.load_dotenv()
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('SECRET_KEY')
-    
+    MAX_CONTENT_LENGTH = 1 * 1024 * 1024
+
     @property
     def SQLALCHEMY_DATABASE_URI(self):
         var = os.getenv('DB_URI')
@@ -15,6 +16,33 @@ class Config(object):
             raise ValueError("DB_URI is not set")
         
         return var
+    
+    @property
+    def AWS_ACCESS_KEY_ID(self):
+        value = os.environ.get("AWS_ACCESS_KEY_ID")
+
+        if not value:
+            raise ValueError("AWS_ACCESS_KEY_ID is not set")
+
+        return value
+    
+    @property
+    def AWS_SECRET_ACCESS_KEY(self):
+        value = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+        if not value:
+            raise ValueError("AWS_SECRET_ACCESS_KEY is not set")
+
+        return value
+    
+    @property
+    def AWS_S3_BUCKET(self):
+        value = os.environ.get("AWS_S3_BUCKET")
+
+        if not value:
+            raise ValueError("AWS_S3_BUCKET is not set")
+
+        return value
         
 
 class Development(Config):
